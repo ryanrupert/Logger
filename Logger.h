9b48853939eda0f28b70b6e11df905855edac3fa
Ryan Rupert <mudge25+code@gmail.com>
2017-11-26 09:31:43 -0800

    Add the class prototype

---
+++ Logger.h
@@ -0,0 +1,26 @@
+/**
+* Created by Ryan Rupert
+* Date: 2017-11-26
+* Source: None
+* This is a class that will used for logging debug info
+*/
+
+#include <iostream>
+#include <ofstream>
+
+template<typename T>
+
+class Logger
+{
+	public:
+	enum Level { ERROR, WARNING, INFO };
+	void setProd(bool prod);
+	void setLevel(Level lev);
+	void message(std::string mes);
+	void varariable(T var);
+
+	private:
+	bool production = false;
+	Level level = ERROR;
+	void log();
+};
